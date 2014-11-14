43f14a0256014e824ef1afd31949c01abbdd6f2f
Girmi Schouten <girmi.schouten@gmail.com>
2014-11-14 11:19:44 +0100

    Main = topmodule, mod_date aangemaakt

---
+++ mod_date.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:43:04 11/14/2014 
+-- Design Name: 
+-- Module Name:    mod_date - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mod_date is
+end mod_date;
+
+architecture Behavioral of mod_date is
+
+begin
+
+
+end Behavioral;
+
